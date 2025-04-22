65095706a8ce54deb3c988188f9897474f6e99d4
uVini-dev <vs28072005@gmail.com>
2025-04-22 18:24:26 +0000

    trabalho final

---
+++ xadrez.C
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main(){
+
+   //movimento da Torre, move 5 casas a direita, utilizando estrutura de repetição while
+
+       printf("Movimento da peça torre:\n");
+   int t = 1;   // variavel
+     while (t <= 5)   // condição para parar de executar o movimento
+     {
+        printf("Direita\n");  // indica o movimento que a peça irá fazer
+        t++;  // incrementação
+     } 
+
+   
+   // movimento do Bispo, move 5 casas na diagonal para cima/direita, utilizando estrutura de repetição do-whle
+
+        printf("\nMovimento da peça bispo:\n");
+    int b = 1;   // variavel
+     do
+     {
+        printf("Cima Direita\n");   // indica o movimento que a peça irá fazer
+        b++;     // incrementação
+     } while (b <= 5); // condição para parar de executar o movimento
+
+
+   // movimento da Rainha, move 8 casas a esquerda, utilizando estrutura de repetição for
+        printf("\nMovimento da peça rainha:\n");  
+        for(int r = 1; r <= 8; r++)  // variavel, condição, incrementação
+     {
+            printf("Esquerda\n");  // indica o movimento que a peça irá fazer
+     }
+
+
+ return 0;
+
+}
